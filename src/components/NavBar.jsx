272ecbbc509232c981d8aada6495adc482e63502
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-09-04 12:25:56 -0300

    feat/ imagens, componentes jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function NavBar() {
+
+    return (
+        
+        <div>
+
+            
+
+        </div>
+
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import React from 'react';
 
-
-
-ReactDOM.render(document.querySelector(".root"));
+ReactDOM.render(<App/>, document.querySelector(".root"));
