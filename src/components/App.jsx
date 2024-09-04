11cb6ba7e21dacacecc2c855175974e940232f70
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-09-04 17:28:02 -0300

    feat/stories

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "./NavBar";
+import StoryList from "./Stories";
 
 export default function App() {
     return (
     <div>
-
-        <NavBar/>
-        
+        <div className="NavBar">
+            <NavBar />
+        </div>
+        <div className="Body">
+            <div>
+                <div>
+                    <StoryList />
+                </div>
+            </div>
+        </div>
     </div>
     )
 }
